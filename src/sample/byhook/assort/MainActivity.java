2c18661d124b25448ce099ec1d11eb1f66dd70ab
byhook <byhook@163.com>
2015-11-27 19:38:53 +0800

    获取App列表

--- src/sample/byhook/assort/MainActivity.java
+++ src/sample/byhook/assort/MainActivity.java
@@ -1,9 +1,19 @@
 package sample.byhook.assort;
 
 import android.app.Activity;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import sample.byhook.bean.AppBean;
 
 public class MainActivity extends Activity {
 
@@ -11,24 +21,37 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
+	private class AppComparator implements Comparator<ApplicationInfo>{
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
+		@Override
+		public int compare(ApplicationInfo left, ApplicationInfo right) {
+			CharSequence leftChar = left.loadLabel(getPackageManager());
+			CharSequence rightChar = right.loadLabel(getPackageManager());
+
+			return 1;
 		}
-		return super.onOptionsItemSelected(item);
 	}
+
+	private void getAllApp(){
+        PackageManager pm = getPackageManager();
+		List<ApplicationInfo> apps = pm.getInstalledApplications(0);
+
+		AppBean[] appBeans = new AppBean[apps.size()];
+
+		int index = 0;
+		CharSequence title;
+		for(ApplicationInfo info : apps){
+			title = info.loadLabel(pm);
+
+			appBeans[index] = new AppBean();
+
+			index++;
+		}
+
+	}
+
 }
---
+++ src/sample/byhook/bean/AppBean.java
@@ -0,0 +1,42 @@
+package sample.byhook.bean;
+
+import android.graphics.drawable.Drawable;
+
+/**
+ * 作者 : byhook
+ * 时间 : 15-11-27.
+ * 邮箱 : byhook@163.com
+ * 功能 :
+ */
+public class AppBean {
+
+    private String letter;
+
+    private String title;
+
+    private Drawable drawable;
+
+    public String getLetter() {
+        return letter;
+    }
+
+    public void setLetter(String letter) {
+        this.letter = letter;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Drawable getDrawable() {
+        return drawable;
+    }
+
+    public void setDrawable(Drawable drawable) {
+        this.drawable = drawable;
+    }
+}
